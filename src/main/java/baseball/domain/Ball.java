c1822e2587352c3a0cb98e79286b5057e9d5bd60
seungin yang <rhfpdk92@naver.com>
2022-04-15 01:02:54 +0900

    refactor: 볼 안에 Number 객체로 변경 및 비교 메서드 변경

--- src/main/java/baseball/domain/Ball.java
+++ src/main/java/baseball/domain/Ball.java
@@ -1,22 +1,36 @@
 package baseball.domain;
 
+import java.util.Objects;
+
 public class Ball {
-    private int position;
-    private int number;
+    private final int position;
+    private final Number number;
 
     public Ball(int position, int number) {
         this.position = position;
-        this.number = number;
+        this.number = new Number(number);
     }
 
     public CompareStatus compare(Ball ball) {
-        if (position == ball.position && number == ball.number) {
+        if (this.equals(ball)) {
             return CompareStatus.STRIKE;
         }
-        if (number == ball.number) {
+        if (this.number.equals(ball.number)) {
             return CompareStatus.BALL;
         }
         return CompareStatus.NOTHING;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Ball ball = (Ball) o;
+        return position == ball.position && Objects.equals(number, ball.number);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(position, number);
+    }
 }
