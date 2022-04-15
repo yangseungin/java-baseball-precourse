c5a9663f27e559306072156a5bf6fae1a03384c4
seungin yang <rhfpdk92@naver.com>
2022-04-16 01:59:40 +0900

    refactor: 볼의 위치는 Position객체로 변경 및 비교 메서드 변경

--- src/main/java/baseball/domain/Ball.java
+++ src/main/java/baseball/domain/Ball.java
@@ -3,11 +3,11 @@ package baseball.domain;
 import java.util.Objects;
 
 public class Ball {
-    private final int position;
+    private final Position position;
     private final Number number;
 
     public Ball(int position, int number) {
-        this.position = position;
+        this.position = new Position(position);
         this.number = new Number(number);
     }
 
@@ -26,7 +26,7 @@ public class Ball {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Ball ball = (Ball) o;
-        return position == ball.position && Objects.equals(number, ball.number);
+        return Objects.equals(position, ball.position) && Objects.equals(number, ball.number);
     }
 
     @Override
